75c06b29dc97a9c492a3a961a1aeef330ec4c909
Dakotah Lambert <49007668+vvulpes0@users.noreply.github.com>
2022-10-31 06:47:14 +0100

    ensure tier-ese only skips identity if it is lonely

--- src/smonoid/sm_ese.c
+++ src/smonoid/sm_ese.c
@@ -4,7 +4,11 @@ sm_ese(struct eggboxes * b, int (*f)(struct eggbox *), int x)
 {
 	struct eggbox * p;
 	if (!b) { return 1; }
-	if (x && !b->is_proper) { b = b->next; }
+	if (x && !b->is_proper && b->box->rows == 1 && b->box->cols == 1
+	    && !b->box->polyeggs)
+	{
+		b = b->next;
+	}
 	for (; b; b = b->next) { if (!f(b->box)) { return 0; } }
 	return 1;
 }
